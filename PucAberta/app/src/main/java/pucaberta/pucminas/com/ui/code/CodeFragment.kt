e9608861139a7e3145aa7f990d18f26225dd74ff
pedrolaviola <pedrolaviola@timcoo.com.br>
2018-09-27 15:17:12 -0300

    Adicionado o gerador de perguntas

--- PucAberta/app/src/main/java/pucaberta/pucminas/com/ui/code/CodeFragment.kt
+++ PucAberta/app/src/main/java/pucaberta/pucminas/com/ui/code/CodeFragment.kt
@@ -1,14 +1,18 @@
 package pucaberta.pucminas.com.ui.code
 
+import android.animation.Animator
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.os.Handler
 import android.support.v4.app.Fragment
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.code_fragment.*
+import kotlinx.android.synthetic.main.question_fragment.*
 import pucaberta.pucminas.com.R
 import java.io.IOException
 import java.io.InputStream
@@ -34,9 +38,21 @@ class CodeFragment : Fragment() {
             if (checkCode()) {
                 val bundle = Bundle()
                 val myJson = inputStreamToString(resources.openRawResource(R.raw.questions))
-                bundle.putString("questions", viewModel.getQuestionsJson(myJson))
-                Navigation.findNavController(activity!!, R.id.myFragment)
-                        .navigate(R.id.action_codeFragment_to_questionFragment, bundle)
+                bundle.putString("questions",
+                        viewModel.getQuestionsJson(myJson, codeField.text.toString()))
+                loadingContainer.visibility  = View.VISIBLE
+                loadingAnimation.addAnimatorListener(object : Animator.AnimatorListener {
+                    override fun onAnimationRepeat(p0: Animator?) {}
+                    override fun onAnimationCancel(p0: Animator?) {}
+                    override fun onAnimationStart(p0: Animator?) {}
+                    override fun onAnimationEnd(p0: Animator?) {
+                        Handler().postDelayed({
+                            Navigation.findNavController(activity!!, R.id.myFragment)
+                                    .navigate(R.id.action_codeFragment_to_questionFragment, bundle)
+                        }, 500)
+                    }
+                })
+                loadingAnimation.playAnimation()
             }
         }
     }
@@ -52,7 +68,7 @@ class CodeFragment : Fragment() {
     }
 
     private fun checkCode(): Boolean {
-        return true
+        return !TextUtils.isEmpty(codeField.text) && codeField.text.length == 4
     }
 
     private fun inputStreamToString(inputStream: InputStream): String {
